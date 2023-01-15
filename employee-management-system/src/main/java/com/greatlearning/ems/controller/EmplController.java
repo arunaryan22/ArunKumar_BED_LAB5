d7d66b1c5f272a5b03bb79f9d8cf8b0c809bcd13
arunaryan22 <113823450+arunaryan22@users.noreply.github.com>
2023-01-15 15:59:49 +0530

    Add files via upload

--- employee-management-system/src/main/java/com/greatlearning/ems/controller/EmplController.java
+++ employee-management-system/src/main/java/com/greatlearning/ems/controller/EmplController.java
@@ -2,7 +2,7 @@ package com.greatlearning.ems.controller;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
+//import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,51 +22,49 @@ public class EmplController {
 		super();
 		this.employeeService = employeeService;
 	}
-	
+
 	@GetMapping("/employees")
 	public String listEmployees(Model model) {
-		List<Employee> employees= employeeService.getAllEmployees();
+		List<Employee> employees = employeeService.getAllEmployees();
 		model.addAttribute("employees", employees);
 		return "employees";
 	}
-	
+
 	@GetMapping("/employees/{id}")
-	public String deleteEmployee(@PathVariable ("id") int id) {
-		
+	public String deleteEmployee(@PathVariable("id") int id) {
+
 		employeeService.deleteEmployeeById(id);
 		return "redirect:/employees";
 	}
-		@GetMapping("/employees/new")
-		public String createEmployeeForm(Model model)
-		{
-		Employee employee=new Employee();
+
+	@GetMapping("/employees/new")
+	public String createEmployeeForm(Model model) {
+		Employee employee = new Employee();
 		model.addAttribute("employee", employee);
 		return "create_employee";
 	}
-		
-		@PostMapping("/employees")
-		public String saveEmployee(@ModelAttribute ("employee") Employee employee )
-		{
-			employeeService.insertEmployee(employee);
-			return "redirect:/employees";
-		}
-		
-		@GetMapping("/employees/edit/{id}")
-		public String editEmployeeForm(@PathVariable("id") int id, Model model) {
-			Employee employee=employeeService.getEmployeeById(id);
-			model.addAttribute("employee", employee);
-			return "edit_employee";
-		}
-		@PostMapping("/employee/{id}")
-		public String updateEmpoyee(@PathVariable("id") int id, @ModelAttribute("employee") Employee employee) {
-		Employee existingEmployee=employeeService.getEmployeeById(id);
+
+	@PostMapping("/employees")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.insertEmployee(employee);
+		return "redirect:/employees";
+	}
+
+	@GetMapping("/employees/edit/{id}")
+	public String editEmployeeForm(@PathVariable("id") int id, Model model) {
+		Employee employee = employeeService.getEmployeeById(id);
+		model.addAttribute("employee", employee);
+		return "edit_employee";
+	}
+
+	@PostMapping("/employees/{id}")
+	public String updateEmpoyee(@PathVariable("id") int id, @ModelAttribute("employee") Employee employee) {
+		Employee existingEmployee = employeeService.getEmployeeById(id);
 		existingEmployee.setFirstName(employee.getFirstName());
 		existingEmployee.setLastName(employee.getLastName());
 		existingEmployee.setEmail(employee.getEmail());
 		employeeService.insertEmployee(existingEmployee);
 		return "redirect:/employees";
-		
-		}
+
+	}
 }
-
-
